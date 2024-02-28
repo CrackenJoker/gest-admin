1cde9c1173b908f6a58404fc2f637136a36b044e
CrackenJoker <joelsoncandomba85@gmail.com>
2024-02-28 17:26:30 +0100

    Primeirma versão do sistema de gestão

---
+++ src/views/chartConfig.ts
@@ -0,0 +1,14 @@
+export const plByMonth = [
+    { name: 'Janeiro', pl: 1, avg: 500, inc: 300 },
+    { name: 'Fevereiro', pl: 2000, avg: 900, inc: 400 },
+    { name: 'Março', pl: 400, avg: 400, inc: 500 },
+    { name: 'Abril', pl: 3100, avg: 1300, inc: 700 },
+    { name: 'Maio', pl: 200, avg: 100, inc: 200 },
+    { name: 'Junho', pl: 600, avg: 400, inc: 300 },
+    { name: 'Julho', pl: 500, avg: 90, inc: 100 },
+    { name: 'Agosto', pl: 500, avg: 90, inc: 100 },
+    { name: 'Setembro', pl: 500, avg: 90, inc: 100 },
+    { name: 'Outubro', pl: 500, avg: 90, inc: 100 },
+    { name: 'Novembro', pl: 500, avg: 90, inc: 100 },
+    { name: 'Dezembro', pl: 500, avg: 90, inc: 100 }
+  ]
